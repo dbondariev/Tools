b1226a30bb2bacf3dcc091562d118c6650c5a810
dbondariev <dima.bondariev@gmail.com>
2022-07-28 13:27:05 +0300

    node modulkes

--- lesson1/task_1/src/scripts/logger/logger.js
+++ lesson1/task_1/src/scripts/logger/logger.js
@@ -1,18 +1,18 @@
 // будет создавать логер
 export const createLogger = name => {
-  // хранилище для логов
-  const logs = [];
+ // хранилище для логов
+ const logs = [];
 
-  // вызов методов
-  return {
-    log(message) {
-      logs.push(`log - ${name} - ${message}`);
-    },
-    error(errorText) {
-      logs.push(`error - ${name} - ${errorText}`);
-    },
-    getLogs() {
-      return logs;
-    },
-  };
+ // вызов методов
+ return {
+  log(message) {
+   logs.push(`log - ${name} - ${message}`);
+  },
+  error(errorText) {
+   logs.push(`error - ${name} - ${errorText}`);
+  },
+  getLogs() {
+   return logs;
+  },
+ };
 };
